3520b5190d10be03e01ad637ad59baa73c6ba93e
Marten Seemann <martenseemann@gmail.com>
2024-10-13 12:08:18 +0800

    WIP add a basic client

---
+++ client.go
@@ -0,0 +1,67 @@
+package connectip
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"net/http"
+	"net/url"
+
+	"github.com/quic-go/quic-go/http3"
+	"github.com/yosida95/uritemplate/v3"
+)
+
+// defaultInitialPacketSize is an increased packet size used for the connection to the proxy.
+// This allows tunneling QUIC connections, which themselves have a minimum MTU requirement of 1200 bytes.
+const defaultInitialPacketSize = 1350
+
+// Dial dials a proxied connection to a target server.
+func Dial(ctx context.Context, conn *http3.ClientConn, template *uritemplate.Template) (*Conn, *http.Response, error) {
+	expandedTemplate, err := template.Expand(uritemplate.Values{})
+	if err != nil {
+		return nil, nil, fmt.Errorf("connect-ip: failed to expand Template: %w", err)
+	}
+
+	u, err := url.Parse(expandedTemplate)
+	if err != nil {
+		return nil, nil, fmt.Errorf("connect-ip: failed to parse URI: %w", err)
+	}
+
+	select {
+	case <-ctx.Done():
+		return nil, nil, context.Cause(ctx)
+	case <-conn.Context().Done():
+		return nil, nil, context.Cause(conn.Context())
+	case <-conn.ReceivedSettings():
+	}
+	settings := conn.Settings()
+	if !settings.EnableExtendedConnect {
+		return nil, nil, errors.New("connect-ip: server didn't enable Extended CONNECT")
+	}
+	if !settings.EnableDatagrams {
+		return nil, nil, errors.New("connect-ip: server didn't enable Datagrams")
+	}
+
+	rstr, err := conn.OpenRequestStream(ctx)
+	if err != nil {
+		return nil, nil, fmt.Errorf("connect-ip: failed to open request stream: %w", err)
+	}
+	if err := rstr.SendRequestHeader(&http.Request{
+		Method: http.MethodConnect,
+		Proto:  requestProtocol,
+		Host:   u.Host,
+		Header: http.Header{capsuleHeader: []string{capsuleProtocolHeaderValue}},
+		URL:    u,
+	}); err != nil {
+		return nil, nil, fmt.Errorf("connect-ip: failed to send request: %w", err)
+	}
+	// TODO: optimistically return the connection
+	rsp, err := rstr.ReadResponse()
+	if err != nil {
+		return nil, nil, fmt.Errorf("connect-ip: failed to read response: %w", err)
+	}
+	if rsp.StatusCode < 200 || rsp.StatusCode > 299 {
+		return nil, rsp, fmt.Errorf("connect-ip: server responded with %d", rsp.StatusCode)
+	}
+	return newProxiedConn(rstr), rsp, nil
+}
